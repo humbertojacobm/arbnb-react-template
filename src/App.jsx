f31b67ce9953defaf5d1032975fc521ebd30a627
humberto.mori <humberto.mori@tektonlabs.com>
2021-05-27 16:22:44 -0500

    adding styles in other file

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,14 @@ import {
   Route,
   Link as RouterLink,
 } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
+import AppStyles from './AppStyles.css';
 import Home from './Componentes/Home';
 import Challenge01 from './Container/Challenge01';
-import './App.css';
-
-const useStyles = makeStyles({
-  root: {
-    marginLeft: '30px',
-  },
-});
 
 function App() {
-  const classes = useStyles();
+  const classes = AppStyles();
   return (
     <Router>
       <Container>
---
+++ src/AppStyles.css.jsx
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const AppStyles = makeStyles({
+  root: {
+    marginLeft: '30px',
+  },
+});
+
+export default AppStyles;
