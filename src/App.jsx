5dde90cacd383cf5f8f056ee577b3e56411e5094
humberto.mori <humberto.mori@tektonlabs.com>
2021-05-27 16:06:52 -0500

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import HelloWord from './Componentes/HelloWord';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink,
+} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import Container from '@material-ui/core/Container';
+import Home from './Componentes/Home';
+import Challenge01 from './Container/Challenge01';
 import './App.css';
 
+const useStyles = makeStyles({
+  root: {
+    marginLeft: '30px',
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <HelloWord />
+    <Router>
+      <Container>
+        <Link component={RouterLink} to="/" className={classes.root}>
+          Home
+        </Link>
+        <Link component={RouterLink} to="/challenge-01" className={classes.root}>Challenge 01</Link>
+        <Switch>
+          <Route path="/challenge-01">
+            <Challenge01 />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/Container/Challenge01.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+const Challenge01 = () => (
+  <>
+    <Box>
+      to-do-hmori
+    </Box>
+  </>
+);
+
+export default Challenge01;
